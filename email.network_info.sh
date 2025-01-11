4a7d11a50ad69a6ace8b7defd82bb6efe7e050c2
michaelb <56658194+mzb2xeo@users.noreply.github.com>
2025-01-11 06:24:30 -0800

    Update email.network_info.sh

--- email.network_info.sh
+++ email.network_info.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-#1/5/25 Production email.network_info.sh
+# 1/10/25 dev from /var/local/email_network_info.sh
+# Script to gather system information and send it via email
+
 # Get current date and time
-current_date_time=$(date +"%A, %B %d, at %Y %H:%M:%S.")
-echo "Current Date & Time: $current_date_time"
+datetime=$(date +"%A, %B %d, at %Y %H:%M:%S.")
+echo "$datetime"
+
 # Check internet connectivity
 if ping -c 1 google.com &> /dev/null; then
     echo "Internet connection is OK"
 else
     echo "No internet connection"
 fi
+
 # Get hostname and gateway
 hostname=$(uname -n)
 gateway=$(ip route show default | awk '{print $3}')
 echo "Hostname: $hostname"
 echo "Gateway: $gateway"
-# Get network interfaces with IP addresses
 
+# Function to get network interfaces with IP addresses
 get_ips() {
     if ip addr show &> /dev/null; then
         echo "Interfaces with IP addresses:"
@@ -32,12 +36,28 @@ get_ips
 
 # Get public IP using curl
 public_ip=$(curl -s https://api.ipify.org)
-if [ "$public_ip" != "" ]; then
+if [ -n "$public_ip" ]; then
     echo "Public IP: $public_ip"
 else
     echo "Failed to get public IP"
 fi
+
+#!/bin/bash
+
+# Function to prompt for an email address
+get_email() {
+    read -p "Please enter your email address: " user_email
+    echo $user_email
+}
+
+# Call the function and store the result in a variable
+email=$(get_email)
+
+# Output the captured email address
+echo "You entered: $email"
+
 # Construct email content
-email_content="$current_date_time\nHostname: $hostname\nInternet Connection: $(ping -c 1 google.com &> /dev/null && echo 'OK' || echo 'No')\n$(get_ips)\nGateway: $gateway\nPublic IP: $public_ip"
-# Email the collected information using mailx
-echo -e "$email_content" | mailx -s "$hostname System Info Report" michael@3mby.com
+email_content="$datetime \nHostname: $hostname\nInternet Connection: $(ping -c 1 google.com &> /dev/null && echo 'OK' || echo 'None')\n$(get_ips)\nGateway: $gateway\nPublic IP: $public_ip"
+
+ Email the collected information using mailx
+echo -e "$email_content" | mailx -s "[$hostname] System Info Report" $email
